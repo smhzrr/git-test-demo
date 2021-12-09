6bda7f41ce1898ac2e576cf203fd1c935a2e8459
sumenghu126 <sumenghu126@126.com>
2021-12-09 15:56:01 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,11 @@
+/**
+ * @author ：smh
+ * @date ：Created in 2021/12/9 15:53
+ * @description：
+ * @version: $
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
